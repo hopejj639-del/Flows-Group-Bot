eba8b061e07e91550468aab5c8ace208f52a9b7f
hopejj639-del <hopejj639@gmail.com>
2026-08-28 12:09:19 +0630

    Update dispatcher.py

--- app/bot/dispatcher.py
+++ app/bot/dispatcher.py
@@ -3,17 +3,20 @@ from aiogram import Dispatcher
 from app.database import AsyncSessionLocal
 from app.bot.middlewares import DatabaseMiddleware
 from app.bot.handlers.general import general_router
-# Import other routers here...
+from app.bot.handlers.admin import admin_router
+from app.bot.handlers.greetings import greetings_router
+from app.bot.handlers.filters import filters_router
 
 def setup_dispatcher() -> Dispatcher:
     dp = Dispatcher()
     
-    # Bind Middleware
+    # Bind Middleware (Database injection)
     dp.update.middleware(DatabaseMiddleware(session_pool=AsyncSessionLocal))
 
-    # Bind Routers
+    # Bind Routers (Order matters! General commands first, then admin, then greetings, then filters)
     dp.include_router(general_router)
-    # dp.include_router(admin_router)
-    # dp.include_router(greetings_router)
+    dp.include_router(admin_router)
+    dp.include_router(greetings_router)
+    dp.include_router(filters_router)
 
     return dp
